beb178c4b77818a93f13bae99eecc940ed8c2e64
compupro <7752184+compupro@users.noreply.github.com>
2018-05-27 19:48:03 -0700

    add single-point rp time getter

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 //set default values
 var daysPerYear = 30;
-var lastDateChange = 1527811200;
-var lastDateEpoch = 1041379200;
+var lastDateChange = 1527811200000;
+var lastDateEpoch = 1041379200000;
 
 function setSettings(){
 	daysPerYear = document.getElementById("daysPerYear").value;
-	lastDateChange = new Date(document.getElementById("lastDateChange").value)/1000;
-	lastDateEpoch = new Date(document.getElementById("lastDateEpoch").value)/1000;
+	lastDateChange = new Date(document.getElementById("lastDateChange").value)/1; //the /1 turns it into a number
+	lastDateEpoch = new Date(document.getElementById("lastDateEpoch").value)/1;
+}
+
+function getTimeOf(irlDate){	
+	return new Date(Math.floor((365/daysPerYear)*((irlDate)-lastDateChange)+lastDateEpoch));
 }
 
 function setTime(){
 	var display = document.getElementById("timeDisplay");
-	var rpUnixTime = new Date(Math.floor((365/daysPerYear)*((Date.now()/1000)-lastDateChange)+lastDateEpoch)*1000);
+	var rpUnixTime = getTimeOf(Date.now());
 	display.innerHTML = "The RP date and time is<br>" + rpUnixTime.toGMTString();
+}
+
+function getSingleRPTime(){
+	var singleDisplay = document.getElementById("singleRPTimeDisplay");
+	var singleIRLTime = document.getElementById("singleTime").value.split(":");
+	singleIRLTime = parseInt(singleIRLTime[0]*60) + parseInt(singleIRLTime[1]); //get selected time in minutes
+	singleIRLTime = new Date((new Date(document.getElementById("singleDate").value)/1) + singleIRLTime*60000)/1; //create a date using selected time in milliseconds plus selected time converted to milliseconds
+	singleDisplay.innerHTML = getTimeOf(singleIRLTime).toGMTString();
 }
 
 setInterval(function(){setTime()}, 1);

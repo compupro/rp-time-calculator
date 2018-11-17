51cefa8ac2a5453dfd5745b1816f2f90dad378ed
compupro <7752184+compupro@users.noreply.github.com>
2018-11-16 20:12:15 -0800

    Make tabs into spaces

--- main.js
+++ main.js
@@ -10,35 +10,35 @@ document.getElementById("lastDateChange").value = new Date(lastDateChange).toISO
 document.getElementById("lastDateEpoch").value = new Date(lastDateEpoch).toISOString().split("T")[0];
 
 function setSettings(){
-	daysPerYear = document.getElementById("daysPerYear").value;
-	lastDateChange = new Date(document.getElementById("lastDateChange").value)/1; //the /1 turns it into a number
-	lastDateEpoch = new Date(document.getElementById("lastDateEpoch").value)/1;
+    daysPerYear = document.getElementById("daysPerYear").value;
+    lastDateChange = new Date(document.getElementById("lastDateChange").value)/1; //the /1 turns it into a number
+    lastDateEpoch = new Date(document.getElementById("lastDateEpoch").value)/1;
 }
 
 function exportSettings(){
-	var paramString = "?";
-	paramString += "daysperyear=" + daysPerYear;
-	paramString += "&lastdatechange=" + lastDateChange;
-	paramString += "&lastdateepoch=" + lastDateEpoch;
-	window.prompt("Copy the following link:", [location.protocol, "//", location.host, location.pathname].join('') + paramString);
+    var paramString = "?";
+    paramString += "daysperyear=" + daysPerYear;
+    paramString += "&lastdatechange=" + lastDateChange;
+    paramString += "&lastdateepoch=" + lastDateEpoch;
+    window.prompt("Copy the following link:", [location.protocol, "//", location.host, location.pathname].join('') + paramString);
 }
 
 function getTimeOf(irlDate){
-	return new Date(Math.floor((365/daysPerYear)*((irlDate)-lastDateChange)+lastDateEpoch));
+    return new Date(Math.floor((365/daysPerYear)*((irlDate)-lastDateChange)+lastDateEpoch));
 }
 
 function setTime(){
-	var display = document.getElementById("timeDisplay");
-	var rpJSTime = getTimeOf(Date.now());
-	display.innerHTML = "The RP date and time is<br>" + rpJSTime.toGMTString();
+    var display = document.getElementById("timeDisplay");
+    var rpJSTime = getTimeOf(Date.now());
+    display.innerHTML = "The RP date and time is<br>" + rpJSTime.toGMTString();
 }
 
 function getSingleRPTime(){
-	var singleDisplay = document.getElementById("singleRPTimeDisplay");
-	var singleIRLTime = document.getElementById("singleTime").value.split(":");
-	singleIRLTime = parseInt(singleIRLTime[0]*60) + parseInt(singleIRLTime[1]); //get selected IRL time in minutes
+    var singleDisplay = document.getElementById("singleRPTimeDisplay");
+    var singleIRLTime = document.getElementById("singleTime").value.split(":");
+    singleIRLTime = parseInt(singleIRLTime[0]*60) + parseInt(singleIRLTime[1]); //get selected IRL time in minutes
     singleIRLTime *= 60000; //make it milliseconds
-	var singleIRLOffset = document.getElementById("singleOffset").value; //get selected IRL timezone in hours
+    var singleIRLOffset = document.getElementById("singleOffset").value; //get selected IRL timezone in hours
     singleIRLOffset *= 3600000; //make it milliseconds
     singleIRLTime -= singleIRLOffset; //subtract the offset from the time to make it UTC
 
@@ -46,7 +46,7 @@ function getSingleRPTime(){
     singleIRLDate = new Date(singleIRLDate)/1; //turn it into js time (milliseconds)
     var dateTime = new Date(singleIRLDate + singleIRLTime)/1;
 
-	singleDisplay.innerHTML = getTimeOf(dateTime).toGMTString();
+    singleDisplay.innerHTML = getTimeOf(dateTime).toGMTString();
 }
 
 setInterval(function(){setTime()}, 1);

076d15c694258b024264f0fc1adfda405ef1fc94
compupro <7752184+compupro@users.noreply.github.com>
2024-01-28 15:19:54 -0800

    cast url params to correct type before using

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //set values according to the URL parameters if it exists, using defaults if they don't
 const pageUrl = new URLSearchParams(location.search);
-let daysPerYear = pageUrl.get("daysperyear") ?? 14;
-let lastDateChange = pageUrl.get("lastdatechange") ?? 1533081600000; //JS time adds three zeroes to UNIX time
-let lastDateEpoch = pageUrl.get("lastdateepoch") ?? 1104537600000;
-let fixedYears = pageUrl.get("fixedyears") ?? false;
+let daysPerYear = pageUrl.get("daysperyear") ? pageUrl.get("daysperyear")/1 : 14;
+let lastDateChange = pageUrl.get("lastdatechange") ? pageUrl.get("lastdatechange")/1 : 1533081600000; //JS time adds three zeroes to UNIX time
+let lastDateEpoch = pageUrl.get("lastdateepoch") ? pageUrl.get("lastdateepoch")/1 : 1104537600000;
+let fixedYears = pageUrl.get("fixedyears") ? pageUrl.get("fixedyears") === "true" : false;
 
 //change the HTML elements to reflect the values
 document.getElementById("daysPerYear").value = daysPerYear;
